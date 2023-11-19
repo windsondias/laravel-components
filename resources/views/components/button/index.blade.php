c563142c74324a5d364ea68dacfc7671be856018
Windson Dias <windson.dsantos@outlook.com>
2023-11-19 12:25:03 -0300

    Fix text color in button

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -3,13 +3,14 @@
 ])
 
 <button
-    {{ $attributes->class([
-        'block w-full h-10 px-3 focus:outline-none text-sm font-medium rounded-lg flex items-center justify-center text-white bg-' . $variant . '-500',
-        'cursor-not-allowed opacity-50' => $attributes->has('disabled'),
-        'hover:bg-' . $variant . '-400' => !$attributes->has('disabled'),
-    ])->merge([
-        'type' => 'button'
-    ]) }}
+        {{ $attributes->class([
+            'block w-full h-10 px-3 focus:outline-none text-sm font-medium rounded-lg flex items-center justify-center bg-' . $variant . '-500',
+            'cursor-not-allowed opacity-50' => $attributes->has('disabled'),
+            'hover:bg-' . $variant . '-400' => !$attributes->has('disabled'),
+            'text-white' => !empty($variant),
+        ])->merge([
+            'type' => 'button'
+        ]) }}
 >
     {{$slot}}
 </button>
