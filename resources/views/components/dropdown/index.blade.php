da6fefaf46922cfb24287652adcf00bda01d6a10
Windson Dias <windson.dsantos@outlook.com>
2023-11-19 12:36:23 -0300

    Change dropdown background color

--- resources/views/components/dropdown/index.blade.php
+++ resources/views/components/dropdown/index.blade.php
@@ -10,7 +10,7 @@
         x-show="open"
         @click.outside="open = false"
         {{ $attributes->class([
-            'absolute flex flex-col w-56 py-2 z-20 mt-1 bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-100 rounded-lg',
+            'absolute flex flex-col w-56 py-2 z-20 mt-1 bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100 rounded-lg',
             'left-0' => $align === 'left',
             'right-0' => $align === 'right',
         ]) }}>
