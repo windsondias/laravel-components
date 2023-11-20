5af6095618b274e3571e5973e28f33bcdc4d5a6a
Windson Dias <windson.dsantos@outlook.com>
2023-11-20 17:11:32 -0300

    Add gap to button

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -4,7 +4,7 @@
 
 <button
         {{ $attributes->class([
-            'block w-full h-10 px-3 focus:outline-none text-sm font-medium rounded-lg flex items-center justify-center bg-' . $variant . '-500',
+            'block w-full gap-2 h-10 px-3 focus:outline-none text-sm font-medium rounded-lg flex items-center justify-center bg-' . $variant . '-500',
             'cursor-not-allowed opacity-50' => $attributes->has('disabled'),
             'hover:bg-' . $variant . '-400' => !$attributes->has('disabled'),
             'text-white' => !empty($variant),
