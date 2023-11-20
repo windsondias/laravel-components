0d41c6ffcd1ba665df2cc2e2f7c142ec1be1fba7
Windson Dias <windson.dsantos@outlook.com>
2023-11-20 09:57:39 -0300

    Change sidebar text color

--- resources/views/components/sidebar/dropdown/item/index.blade.php
+++ resources/views/components/sidebar/dropdown/item/index.blade.php
@@ -9,7 +9,7 @@
     {{ $attributes->class([
         'flex items-center gap-3 p-2 pl-14 w-60 text-base font-medium  rounded-lg transition duration-75 group hover:bg-gray-100 dark:hover:bg-gray-700',
         'text-gray-900 dark:text-white' => !request()->routeIs($routeIs),
-        'text-purple-500 dark:text-purple-500' => request()->routeIs($routeIs),
+        'text-primary-500 dark:text-primary-200' => request()->routeIs($routeIs),
     ]) }}
 >
     {{$slot}}
--- resources/views/components/sidebar/item/index.blade.php
+++ resources/views/components/sidebar/item/index.blade.php
@@ -8,7 +8,7 @@
     {{ $attributes->class([
         'flex items-center gap-4 w-60 h-10 p-2 text-base font-medium rounded-lg group hover:bg-gray-100 dark:hover:bg-gray-700',
         'text-gray-900 dark:text-white' => !request()->routeIs($routeIs),
-        'text-purple-500 dark:text-purple-500' => request()->routeIs($routeIs),
+        'text-primary-500 dark:text-primary-200' => request()->routeIs($routeIs),
     ]) }}
 >
     {{$slot}}
