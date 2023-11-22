49c15ad3189d664d2c9c7c26706053dedc0ebcd5
Windson Dias <windson.dsantos@outlook.com>
2023-11-22 11:52:44 -0300

    Add size to buttons

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,13 +1,18 @@
 @props([
-    'variant' => ''
+    'variant' => '',
+    'size' => 'md'
 ])
 
 <button
         {{ $attributes->class([
-            'block w-full gap-2 h-10 px-3 focus:outline-none text-sm font-medium rounded-lg flex items-center justify-center bg-' . $variant . '-500',
+            'block w-full gap-2 focus:outline-none font-medium flex rounded items-center justify-center bg-' . $variant . '-500',
             'cursor-not-allowed opacity-50' => $attributes->has('disabled'),
             'hover:bg-' . $variant . '-400' => !$attributes->has('disabled'),
             'text-white' => !empty($variant),
+            'h-8 p-2 text-sm' => $size == 'sm',
+            'h-10 p-3 text-sm' => $size == 'md',
+            'h-12 p-4' => $size == 'lg',
+            'h-14 p-5' => $size == 'xl',
         ])->merge([
             'type' => 'button'
         ]) }}
